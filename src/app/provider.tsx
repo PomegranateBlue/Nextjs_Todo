4bff4a91b5c161de4eadcb18d915d22134f7c733
bluepome98 <wpdn1998@gmail.com>
2025-03-18 15:32:32 +0900

    브랜치 정리 및 프로바이더 설정

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,4 +1,13 @@
-"use client"
+"use client";
 
-import { QueryClient,QueryClientProvider } from "@tanstack/react-query"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const Provider = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default Provider;
